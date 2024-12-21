911ca1e8b6d34c65c69c4376191f65737b015fd0
emilsvennesson <emilsvennesson@users.noreply.github.com>
2024-12-21 15:26:55 +0100

    Revert "remove apikey"
    This reverts commit d905a6af9f4d1822edf7c60e7160944275ba9308.

--- rootfs/tpls/rtorrent-cross-seed.sh
+++ rootfs/tpls/rtorrent-cross-seed.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -XPOST http://cross-seed:2468/api/webhook --data-urlencode "infoHash=$2"
+curl -XPOST http://cross-seed:2468/api/webhook?apikey=92e6a17bdfdffb260df88f4b134f5ea6b8534ea810c6f69f --data-urlencode "infoHash=$2"
